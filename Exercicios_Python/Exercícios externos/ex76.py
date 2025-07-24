1f9dcc91297a21b2e636448af4036acfa3c1e1cf
EmanuelleFarias <emma.dfarias@gmail.com>
2025-07-24 14:32:45 -0300

    ok

--- Exercicios_Python/Exercícios externos/ex76.py
+++ Exercicios_Python/Exercícios externos/ex76.py
@@ -6,3 +6,24 @@ na sequência
 No final, mostre uma listagem de precos,
 organizando os dados em forma tabular
 '''
+print('⏔' *29)
+print(f"{'Lojinha da Melzinha':^43}")
+print('⏔' *29)
+print('_'*43)
+produtos = ('Lápis', 1.00,
+         'Caneta', 1.50,
+         'Borracha', 0.70,
+         'Mesa', 1000.00,
+         'Teclado', 600.00,
+         'Monitor', 3000.00,
+         'Caderno', 50.00,
+         'Mouse', 60.00)
+
+for p in range(0, len(produtos),2):
+    nome = produtos[p]
+    preço = produtos[p+1]
+    linha = f"{nome:•<30} R$ {preço:>7.2f}"
+    print(linha.center(43))
+
+print('—'*43)
+print('⏔'*29)
