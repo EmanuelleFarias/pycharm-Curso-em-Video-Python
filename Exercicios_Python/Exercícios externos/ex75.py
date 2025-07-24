97495c1f9b14485c264f049647b412e63aadbdcc
EmanuelleFarias <emma.dfarias@gmail.com>
2025-07-24 14:39:25 -0300

    ok

--- Exercicios_Python/Exercícios externos/ex75.py
+++ Exercicios_Python/Exercícios externos/ex75.py
@@ -16,15 +16,20 @@ tup_num = tuple(lis_num)
 print(tup_num)
 
 if 9 in tup_num:
-    print(f'⫸ O número 9 apareceu {tup_num.count(9)} vez(es).')
+    print(f'\n⫸ O número 9 apareceu {tup_num.count(9)} vez(es).')
+else:
+    print('⫸ O número 9 não foi digitado nenhuma vez.')
 
 if 3 in tup_num:
     print(f'⫸ O primeiro número 3 apareceu na posição {tup_num.index(3)+1}.')
+else:
+    print('⫸ O número 3 não foi digitado nenhuma vez.')
 
 print(f'⫸Foram digitados os seguintes números pares:', end=' ')
 for v in tup_num:
     if v % 2 == 0:
         print(f'{v}',end=' ')
+print('')
 
 
 
