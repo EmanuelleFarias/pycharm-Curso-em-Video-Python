c48bc361f829fc36f0a2b5541667f457bf149a8c
EmanuelleFarias <emma.dfarias@gmail.com>
2025-07-27 18:51:24 -0300

    primo

---
+++ Exercicios_Python/Exercícios externos/Loops/21-loop-NUMERO PRIMO.py
@@ -0,0 +1,15 @@
+#Faça um programa que peça um número inteiro e determine se ele é ou não um número primo. Um número primo é aquele que é divisível somente por ele mesmo e por 1.
+num = int(input('Digite um número: '))
+
+div = []
+cont = 0
+
+print(f"\n{num} é divisível por ele mesmo e mais ", end='')
+for i in range(1, 10):
+    if num % i == 0:
+        div.append(i)
+        cont+= 1
+    tupla = tuple(div)
+print(f'{cont} números: ')
+print((', '.join(str(i) for i in tupla)), ".\nLogo, NÃO É UM NÚMERO PRIMO."if cont > 2 else f".\nLogo, É UM NÚMERO PRIMO.")
+
